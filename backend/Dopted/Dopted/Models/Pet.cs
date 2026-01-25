dfeea0b9688732c602595e7c1809d9a1ff9658f5
Bryn Norwood <norwoodbryn@gmail.com>
2026-01-25 09:44:26 -0400

    added first 2 models.

---
+++ backend/Dopted/Dopted/Models/Pet.cs
@@ -0,0 +1,34 @@
+﻿namespace Dopted.Models
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string Species { get; set; } = null!;   //Dog or Cat
+
+        public string Breed { get; set; } = null!;
+
+        public int AgeYears { get; set; }
+
+        public string Sex { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public string PhotoUrl { get; set; } = null!;
+
+        public string Location { get; set; } = null!;  
+
+        public bool IsAdopted { get; set; }
+
+        // Contact info for this listing (from the owner’s form)
+        public string ContactEmail { get; set; } = null!;
+        public string? ContactWebsite { get; set; }
+
+        // Owner (user / shelter who created the listing)
+        public int OwnerUserAccountId { get; set; }
+
+        public UserAccount Owner { get; set; } = null!;
+    }
+}
---
+++ backend/Dopted/Dopted/Models/UserAccount.cs
@@ -0,0 +1,20 @@
+﻿namespace Dopted.Models
+{
+    public class UserAccount
+    {
+        public int Id { get; set; }
+
+      
+        public string DisplayName { get; set; } = null!;
+
+       
+        public string Email { get; set; } = null!;
+
+        //(if they’re a shelter or have a site)
+        public string? WebsiteUrl { get; set; }
+
+        // Navigation: all pets this user has listed
+        public ICollection<Pet> Pets { get; set; } = new List<Pet>();
+
+    }
+}
