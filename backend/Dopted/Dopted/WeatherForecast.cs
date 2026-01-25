821de0390338feb12b4f6d40eae1fcfc660f77a9
Bryn Norwood <norwoodbryn@gmail.com>
2026-01-25 09:32:45 -0400

    initial commit

---
+++ backend/Dopted/Dopted/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Dopted
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
