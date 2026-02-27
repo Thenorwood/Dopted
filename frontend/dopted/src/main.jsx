bd5fd3d470cceb15126696aaf8f0b45a2ffca411
emill <emill@EMILLYH>
2026-02-27 08:22:34 -0400

    adding routes

--- frontend/dopted/src/main.jsx
+++ frontend/dopted/src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './styles/globals.css'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import About from './pages/About'
+import PetProfile from './pages/PetProfile'
+import Register from './pages/Register'
+import Browse from './pages/Browse'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/pet/:id" element={<PetProfile />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/browse" element={<Browse />} />
+      </Routes>
+    </Router>
   </StrictMode>,
 )
