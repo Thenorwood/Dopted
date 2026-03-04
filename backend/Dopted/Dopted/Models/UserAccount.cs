311f8648c8769004a127b78de090effaeeb9b4d0
Bryn Norwood <norwoodbryn@gmail.com>
2026-03-04 08:35:41 -0400

    enum missing

--- backend/Dopted/Dopted/Models/UserAccount.cs
+++ backend/Dopted/Dopted/Models/UserAccount.cs
@@ -5,11 +5,15 @@
         public int Id { get; set; }//updated
 
         public string DisplayName { get; set; } = null!;
-        public string Email { get; set; } = null!;
-        public string Password { get; set; } = null!;
 
-        public string? WebsiteUrl { get; set; }
-        //
+       
+        public string Email { get; set; } = null!;
+
+        // what kind of account this is
+        public UserType UserType { get; set; } = UserType.Regular;
+
+        //if they’re a shelter or have a site
+        public string? WebsiteUrl { get; set; }        
 
         public ICollection<Pet> Pets { get; set; } = new List<Pet>();
     }
---
+++ backend/Dopted/Dopted/Models/UserType.cs
@@ -0,0 +1,8 @@
+﻿namespace Dopted.Models
+{
+    public enum UserType
+    {
+        Regular,
+        Admin
+    }
+}
